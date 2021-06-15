13899058f530ffee14e32dd72f9bbbeea54b832d
Erika Doyle Navara <erika.doyle@microsoft.com>
2021-06-15 13:47:45 -0700

    Add source code

---
+++ VS2013/FabrikamFiber.CallCenter/FabrikamFiber.Web.Tests/App_Start/NinjectMVC3Test.cs
@@ -0,0 +1,104 @@
+﻿namespace FabrikamFiber.Web.Tests.App_Start
+{
+    using System;
+    using FabrikamFiber.Web.App_Start;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+    using Moq;
+    using Ninject;
+    using Ninject.Web.Mvc;
+    using FabrikamFiber.Web.Data;
+
+    /// <summary>
+    /// Summary description for NinjectMVC3Test
+    /// </summary>
+    [TestClass]
+    public class NinjectMVC3Test
+    {
+        [TestMethod]
+        public void ItShouldHaveASingletonReferenceToCurrentInstance()
+        {
+            Assert.AreEqual(TestableNinjectMVC3.SingletonInstance, TestableNinjectMVC3.SingletonInstance);
+        }
+
+        [TestMethod]
+        public void ItShouldCreateAKernelForTheApplication()
+        {
+            var bootstrapperMock = new Mock<IBootstrapper>();
+            var ninject = new TestableNinjectMVC3(bootstrapperMock.Object);
+
+            var kernel = ninject.CreateKernel();
+
+            Assert.IsInstanceOfType(kernel, typeof(StandardKernel));
+        }
+
+        [TestMethod]
+        public void ItShouldCallBoostrapperInitializeWithCreateKernelAsParameterWhenStartingInstance()
+        {
+            var bootstrapperMock = new Mock<IBootstrapper>();
+            var ninject = new TestableNinjectMVC3(bootstrapperMock.Object);
+
+            bootstrapperMock.Setup(b => b.Initialize(It.Is<Func<IKernel>>(f => f == ninject.CreateKernel)))
+                            .Verifiable();
+           
+            ninject.ExecuteInnerStart();
+
+            bootstrapperMock.VerifyAll();
+        }
+
+        [TestMethod]
+        public void ItShouldCallBoostrapperShutdownWhenStoppingInstance()
+        {
+            var bootstrapperMock = new Mock<IBootstrapper>();
+            var ninject = new TestableNinjectMVC3(bootstrapperMock.Object);
+
+            bootstrapperMock.Setup(b => b.ShutDown()).Verifiable();
+
+            ninject.ExecuteInnerShutDown();
+
+            bootstrapperMock.VerifyAll();
+        }
+
+
+        [TestMethod]
+        public void ItShouldRegisterTheProperTypeMapping()
+        {    
+            var bootstrapperMock = new Mock<IBootstrapper>();
+            var ninject = new TestableNinjectMVC3(bootstrapperMock.Object);
+            
+            var kernel = new StandardKernel();
+            ninject.RegisterServices(kernel);
+
+            Assert.AreEqual(typeof(CustomerRepository), kernel.Get<ICustomerRepository>().GetType());
+            Assert.AreEqual(typeof(EmployeeRepository), kernel.Get<IEmployeeRepository>().GetType());
+            Assert.AreEqual(typeof(ServiceTicketRepository), kernel.Get<IServiceTicketRepository>().GetType());
+            Assert.AreEqual(typeof(ServiceLogEntryRepository), kernel.Get<IServiceLogEntryRepository>().GetType());
+            Assert.AreEqual(typeof(AlertRepository), kernel.Get<IAlertRepository>().GetType());
+            Assert.AreEqual(typeof(MessageRepository), kernel.Get<IMessageRepository>().GetType());
+            Assert.AreEqual(typeof(ScheduleItemRepository), kernel.Get<IScheduleItemRepository>().GetType());
+        }
+
+
+        internal class TestableNinjectMVC3 : NinjectMVC3
+        {
+            public TestableNinjectMVC3(IBootstrapper bootstrapper)
+                : base(bootstrapper)
+            {
+            }
+
+            public static NinjectMVC3 SingletonInstance
+            {
+                get { return NinjectMVC3.Instance; }
+            }
+
+            public void ExecuteInnerStart()
+            {
+                this.InnerStart();
+            }
+
+            public void ExecuteInnerShutDown()
+            {
+                this.InnerShutDown();
+            }
+        }
+    }
+}
---
+++ VS2013/FabrikamFiber.CallCenter/FabrikamFiber.Web.UITests/CodedUITest1.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+using System.Windows.Input;
+using System.Windows.Forms;
+using System.Drawing;
+using Microsoft.VisualStudio.TestTools.UITesting;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Microsoft.VisualStudio.TestTools.UITest.Extension;
+using Keyboard = Microsoft.VisualStudio.TestTools.UITesting.Keyboard;
+
+
+namespace FabrikamFiber.Web.UITests
+{
+    /// <summary>
+    /// Summary description for CodedUITest1
+    /// </summary>
+    //[CodedUITest]
+    public class CodedUITest1
+    {
+        public CodedUITest1()
+        {
+        }
+
+        [DataSource("Microsoft.VisualStudio.TestTools.DataSource.TestCase", "http://vsalm:8080/tfs/fabrikamfibercollection;FabrikamFiber", "79", DataAccessMethod.Sequential), TestMethod]
+        public void CodedUITestMethod1()
+        {
+            // To generate code for this test, select "Generate Code for Coded UI Test" from the shortcut menu and select one of the menu items.
+            // For more information on generated code, see http://go.microsoft.com/fwlink/?LinkId=179463
+            this.UIMap.Loadhttp127001100();
+            this.UIMap.ClickonCustomerslink();
+            this.UIMap.ClickonCreateNewbutton();
+            this.UIMap.EntercustomerdetailsFirstNameLastNameStreetCityStateZipParams.UIFirstNameEditText = TestContext.DataRow["FirstName"].ToString();
+            this.UIMap.EntercustomerdetailsFirstNameLastNameStreetCityStateZipParams.UILastNameEditText = TestContext.DataRow["LastName"].ToString();
+            this.UIMap.EntercustomerdetailsFirstNameLastNameStreetCityStateZipParams.UIStreetEditText = TestContext.DataRow["Street"].ToString();
+            this.UIMap.EntercustomerdetailsFirstNameLastNameStreetCityStateZipParams.UICityEditText = TestContext.DataRow["City"].ToString();
+            this.UIMap.EntercustomerdetailsFirstNameLastNameStreetCityStateZipParams.UIStateEditText = TestContext.DataRow["State"].ToString();
+            this.UIMap.EntercustomerdetailsFirstNameLastNameStreetCityStateZipParams.UIZipEditText = TestContext.DataRow["Zip"].ToString();
+            this.UIMap.EntercustomerdetailsFirstNameLastNameStreetCityStateZip();
+            this.UIMap.ClickCreate();
+        }
+
+        #region Additional test attributes
+
+        // You can use the following additional attributes as you write your tests:
+
+        ////Use TestInitialize to run code before running each test 
+        //[TestInitialize()]
+        //public void MyTestInitialize()
+        //{        
+        //    // To generate code for this test, select "Generate Code for Coded UI Test" from the shortcut menu and select one of the menu items.
+        //    // For more information on generated code, see http://go.microsoft.com/fwlink/?LinkId=179463
+        //}
+
+        ////Use TestCleanup to run code after each test has run
+        //[TestCleanup()]
+        //public void MyTestCleanup()
+        //{        
+        //    // To generate code for this test, select "Generate Code for Coded UI Test" from the shortcut menu and select one of the menu items.
+        //    // For more information on generated code, see http://go.microsoft.com/fwlink/?LinkId=179463
+        //}
+
+        #endregion
+
+        /// <summary>
+        ///Gets or sets the test context which provides
+        ///information about and functionality for the current test run.
+        ///</summary>
+        public TestContext TestContext
+        {
+            get
+            {
+                return testContextInstance;
+            }
+            set
+            {
+                testContextInstance = value;
+            }
+        }
+        private TestContext testContextInstance;
+
+        public UIMap UIMap
+        {
+            get
+            {
+                if ((this.map == null))
+                {
+                    this.map = new UIMap();
+                }
+
+                return this.map;
+            }
+        }
+
+        private UIMap map;
+    }
+}
